b3cc2c7c3e7a02ab21e94a9c5dbd80b352ef6bae
Aastle Stephno <stephno2003@gmail.com>
2023-02-24 16:13:08 +0530

    Code editor component added Just javascript for now

---
+++ client/src/pages/GameRoom.jsx
@@ -0,0 +1,12 @@
+import CodeEditor from "../components/codeEditor";
+
+const Coding_Room = () => {
+	return (
+		<div>
+			<h1>Coding Room</h1>
+			<CodeEditor />
+		</div>
+	);
+};
+
+export default Coding_Room;
