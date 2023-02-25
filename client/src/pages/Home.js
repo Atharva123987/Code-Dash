244a63c89429f27347687330520edaae6468b38a
Pankaj Singh Sirari <Pankajsirari222@gmail.com>
2023-02-25 11:40:12 +0530

    Minor changes

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -11,10 +11,20 @@ const socket = io.connect("http://localhost:3001")
 const Home = () =>{
   const [username, setUsername] = useState("")
   const [roomId, setRoomId] = useState("")
+  const [isVisible, setIsVisible] = useState(true);
 
+  const toggleVisibility = () => {
+    setIsVisible((prevVisibility) => !prevVisibility);
+  };
   const joinRoom = () =>{
   socket.emit("join_room", [roomId,username]);// sending join_room request from front end to backend
   }
+
+  function handleClick() {
+    toggleVisibility();
+    joinRoom();
+  }
+
 
   const generateRoomId = () =>{
     setRoomId(Math.floor(Math.random() * 9000) + 1000)
@@ -40,6 +50,7 @@ const Home = () =>{
     return(
         <>
         <NavBar/>
+        {isVisible? 
     <div className="home-body flex flex-col items-center  ">
       {/* <img src={bgImg} alt="Logo" style={{width:"110vh", }}/> */}
       <h1 className="text-4xl font-bold mb-4 mt-10 text-white">Welcome to Code Dash!</h1>
@@ -49,12 +60,13 @@ const Home = () =>{
       <p className="text-white border-solid border-2 border-orange-50 p-5"><input maxLength={4}  type='text' value={roomId} style={{color:"black"}} onChange={(e)=>{setRoomId(e.target.value)}}/></p>
       <p className="text-white border-solid border-2 border-orange-50 p-5"><input   type='text' value={username} style={{color:"black"}} onChange={(e)=>{setUsername(e.target.value)}}/></p>
       <button className=" px-8 py-2 rounded-lg bg-blue-700 text-white font-bold hover:bg-blue-600" onClick={generateRoomId}>Generate New Link</button>
-      <button className=" px-8 py-2 rounded-lg bg-blue-700 text-white font-bold hover:bg-blue-600" onClick={joinRoom}>Join</button>
+      <button className=" px-8 py-2 rounded-lg bg-blue-700 text-white font-bold hover:bg-blue-600" onClick={handleClick}>Join</button>
 
-      <GameRoom socket={socket} username={username} roomId={roomId}/>
+      {/* <GameRoom socket={socket} username={username} roomId={roomId}/> */}
 
     </div>
     </div>
+      :<GameRoom socket={socket} username={username} roomId={roomId}/> }
         </>
     )
 }
