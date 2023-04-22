08e5ae7b417799f2f35fc98168737f61496fb993
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-22 22:24:38 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -62,6 +62,8 @@ const Home = () =>{
       </p>
     </div>
     <div class="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
+      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg">Sign in with Google</button>
+      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg">Sign in with GitHub</button>
       <div class="relative flex-grow w-full">
         <label for="full-name" class="leading-7 text-sm text-white">Full Name</label>
         <input type="text" id="full-name" name="full-name" class="w-full bg-transparent bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
@@ -70,7 +72,7 @@ const Home = () =>{
         <label for="email" class="leading-7 text-sm text-white">Email</label>
         <input type="email" id="email" name="email" class="w-full bg-transparent bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
       </div>
-      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-blue-300 rounded text-lg">Button</button>
+      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-blue-300 rounded text-lg">Register</button>
     </div>
   </div>
 </section>
