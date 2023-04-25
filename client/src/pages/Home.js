2ec69a508689389ca544119cd5e3dafdc9789c3b
Atharva123987 <atharva.amberkar@gmail.com>
2023-04-25 16:14:43 +0530

    Icons

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,8 @@ import {useState} from 'react'
 import './home.css'
 import io from 'socket.io-client'
 import GameRoom from "./GameRoom";
+import {FcGoogle} from 'react-icons/fc'
+import {BsGithub} from 'react-icons/bs'
 const socket = io.connect("http://localhost:3001")
 
 const Home = () =>{
@@ -62,8 +64,8 @@ const Home = () =>{
       </p>
     </div>
     <div class="flex  lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end">
-      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg">Sign in with Google</button>
-      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg">Sign in with GitHub</button>
+      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg"><FcGoogle/>Sign in with Google</button>
+      <button class="text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-slate-300 transition-all active:bg-slate-600 active:text-white rounded text-lg"><BsGithub/>Sign in with GitHub</button>
       <div class="relative flex-grow w-full">
         <label for="full-name" class="leading-7 text-sm text-white">Full Name</label>
         <input type="text" id="full-name" name="full-name" class="w-full bg-transparent bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-black focus:ring-2 focus:ring-indigo-200 text-base outline-none text-white py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
